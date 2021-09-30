cbba7c534d3668c9e7a0a901182c33ab5037595b
Rushikesh2503 <rushikeshbambadkar7@gmail.com>
2021-09-30 14:45:49 +0530

    user model change

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -6,6 +6,13 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: false },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    pic: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/rsbrsb/image/upload/v1632979599/blank-profile-picture-973460_640_nqo8um.png",
+    },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
   },
   {
     versionKey: false,
