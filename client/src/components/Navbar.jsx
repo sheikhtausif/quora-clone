dbddd8eddc5b9ca14a8cfa425ad23ac6a1552666
Mohd Tausif <mohammadtausif14@gmail.com>
2021-09-30 08:35:07 +0530

    navbar almost completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,49 +8,121 @@ import Space from '../svg/Space'
 import Notification from '../svg/Notification'
 import Search from '../svg/Search'
 import Language from '../svg/Language'
+import Message from '../svg/Message'
+import Ad from '../svg/Ad'
+import Dollar from '../svg/Dollar'
+import State from '../svg/State'
+import Bookmark from '../svg/Bookmark'
+import Draft from '../svg/Draft'
+import Modal from '@material-ui/core/Modal';
+import { GrNext } from 'react-icons/gr';
 
-const Navbar = () => {
-    return (
-        <div className={styles.navbar}>
-            <nav className={styles.nav_flex}>
-                <div className={styles.left}>
-                    <div className={styles.icons}>
-                        <Logo />
-                    </div>
-                    <div className={styles.icons}>
-                        <Home />
-                        {/* <small>Home</small> */}
-                    </div>
-                    <div className={styles.icons}>
-                        <Following />
-                        {/* <small>Home</small> */}
-                    </div>
-                    <div className={styles.icons}>
-                        <Answer />
-                        {/* <small>Answer</small> */}
-                    </div>
-                    <div className={styles.icons}>
-                        <Space />
-                        {/* <small>Space</small> */}
-                    </div>
-                    <div className={styles.icons}>
-                        <Notification />
-                        {/* <small>Notification</small> */}
-                    </div>
+const Navbar = ({ handleTheme }) => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+
+    const nav = [
+        { icon: <Logo />, text: "" },
+        { icon: <Home />, text: "Home" },
+        { icon: <Following />, text: "Followings" },
+        { icon: <Answer />, text: "Answers" },
+        { icon: <Space />, text: "Space" },
+        { icon: <Notification />, text: "Notifications" },
+    ]
+    const profile_details = [
+        { icon: <Message />, text: "Message" },
+        { icon: <Ad />, text: "Create Ad" },
+        { icon: <Dollar />, text: "Monetization" },
+        { icon: <State />, text: "Your content & state" },
+        { icon: <Bookmark />, text: "Bookmarks" },
+        { icon: <Draft />, text: "Drafts" },
+    ]
+
+    const body = (
+        <div className={styles.modal_body}>
+            <div className={styles.profile_div}>
+                <div className={styles.profile_img}>T</div>
+                <div>
+                    <h3>Mohd Tausif</h3>
+                    <GrNext />
                 </div>
-                <div className={styles.right}>
-                    <div className={styles.inp_box}>
-                        <div>
-                            <Search />
-                            <input type="text" placeholder="Search Quora" />
-                        </div>
+            </div>
+            <hr style={{ color: 'grey' }} />
+            <div>
+                {profile_details.map((el, i) => (
+                    <div key={i} className={styles.profile_details}>
+                        {el.icon}
+                        <p>{el.text}</p>
                     </div>
-                    <div className={styles.user_circle}>T</div>
-                    <span><Language /></span>
-                    <div className={styles.add_ques}>Add question</div>
+                ))}
+            </div>
+            <hr style={{ color: 'grey' }} />
+            <div>
+                <div className={styles.theme}>
+                    <p>Dark mode</p>
+                    <p className={styles.toggle_theme} onClick={handleTheme}>OFF</p>
                 </div>
-            </nav>
+                <p>Settings</p>
+                <p>Languages</p>
+                <p>Help</p>
+                <p>Logout</p>
+            </div>
+            <div className={styles.footer}>
+                <p>About</p>
+                <p>Careers</p>
+                <p>Terms</p>
+                <p>Privacy</p>
+                <p>Acceptable Use</p>
+                <p>Business</p>
+                <p>Press</p>
+                <p>Your Ad Choices</p>
+            </div>
         </div>
+    );
+
+    return (
+        <>
+            <div className={styles.navbar}>
+                <nav className={styles.nav_flex}>
+                    <div className={styles.left}>
+                        {nav.map((el, i) => (
+                            <div key={i} className={styles.icons}>
+                                {el.icon}
+                            </div>
+                        ))}
+                    </div>
+                    <div className={styles.right}>
+                        <div className={styles.inp_box}>
+                            <div>
+                                <Search />
+                                <input type="text" placeholder="Search Quora" />
+                            </div>
+                        </div>
+                        <div className={styles.user_circle} onClick={handleOpen}>T</div>
+                        <span><Language /></span>
+                        <div className={styles.add_ques}>Add question</div>
+                    </div>
+                </nav>
+            </div>
+            <div>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description">
+                    {body}
+                </Modal>
+            </div>
+        </>
     )
 }
 
--- client/src/svg/Bookmark.jsx
+++ client/src/svg/Bookmark.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Bookmark = () => {
     return (
-        <svg width="24px" height="24px" viewBox="0 0 24 24"><g className="icon_svg-stroke icon_svg-fill" strokeWidth="1.5" stroke="#666" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"><path d="M5,12 L8,12 L8,20 L5,20 L5,12 Z M10.5,4 L13.5,4 L13.5,20 L10.5,20 L10.5,4 Z M16,7 L19,7 L19,20 L16,20 L16,7 Z"></path></g></svg>
+        <svg width="24px" height="24px" viewBox="0 0 24 24"><g className="icon_svg-stroke" strokeWidth="1.5" stroke="#666" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"><polygon className="icon_svg-fill" points="10.5012125 16 5.00242498 20 5 8 16 8 16 20"></polygon><polyline points="8.00018899 5.92307692 8 5 19 5 19 17 18.0028546 16.2746434"></polyline></g></svg>
     )
 }
 
