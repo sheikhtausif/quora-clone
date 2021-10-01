9640dd69d6cad1c5b31eab05915dbbc6f39cc57b
Rushikesh2503 <rushikeshbambadkar7@gmail.com>
2021-10-01 13:06:22 +0530

    changes path

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const { MONGOURI } = require("../keys");
+// const { MONGOURI } = require("../keys");
 
 module.exports = () => {
-  return mongoose.connect(MONGOURI);
+  return mongoose.connect("mongodb://localhost:27017/quoradbs");
 };
